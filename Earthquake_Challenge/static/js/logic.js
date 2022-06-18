65d50455f8a43317a311f7d71d0182e56f06a8e2
michelaziemer-YzKkEV <michelaziemer@gmail.com>
2022-06-18 11:52:29 -0500

    final

--- Earthquake_Challenge/static/js/logic.js
+++ Earthquake_Challenge/static/js/logic.js
@@ -1,29 +1,46 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	accessToken: API_KEY
+var streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  maxZoom: 18,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
 });
 
-// We create the second tile layer that will be the background of our map.
+var light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+});
+
+var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+});
+
+var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "satellite-v9",
+  accessToken: API_KEY
+});
+
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
 
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-	center: [40.7, -94.5],
-	zoom: 3,
-	layers: [streets]
-});
 
 // Create a base layer that holds all three maps.
 let baseMaps = {
+  "Dark": dark,
+  "Light": light,
+  "Satellite": satellite,
   "Streets": streets,
   "Satellite": satelliteStreets
   
@@ -41,20 +58,26 @@ let overlays = {
   "Major Earthquakes": majorEarthquakes
 };
 
+var map = L.map("mapid", {
+	center: [30, -13],
+	zoom: 3,
+  layers: [dark, allEarthquakes, tectonic] // default
+});
+
 // Then we add a control to the map that will allow the user to change which
 // layers are visible.
 L.control.layers(baseMaps, overlays).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-
+  
   // This function returns the style data for each of the earthquakes we plot on
   // the map. We pass the magnitude of the earthquake into two separate functions
   // to calculate the color and radius.
   function styleInfo(feature) {
     return {
       opacity: 1,
-      fillOpacity: 1,
+      fillOpacity: .5,
       fillColor: getColor(feature.properties.mag),
       color: "#000000",
       radius: getRadius(feature.properties.mag),
@@ -103,20 +126,36 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     style: styleInfo,
      // We create a popup for each circleMarker to display the magnitude and location of the earthquake
      onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      //add timestamp
+        var unix_timestamp = feature.properties.time;
+        var date = new Date(unix_timestamp).toLocaleDateString("en-US");
+        var time = new Date(unix_timestamp).toLocaleTimeString("en-US");
+      layer.bindPopup("Date: " + date + "<br>Time: " + time + "<br>Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      // event listeners
+      layer.on({
+        mouseover: function(event) {
+            let layer_target = event.target;
+            layer_target.setStyle({ fillOpacity: 1 });
+        },
+        mouseout: function(event) {
+            let layer_target = event.target;
+            layer_target.setStyle({ fillOpacity: 0.5 });
+        },
+        click: function(event) {
+            myMap.fitBounds(event.target.getBounds());
+        }
+      });
     }
   }).addTo(allEarthquakes);
 
-  // Then we add the earthquake layer to our map.
-  allEarthquakes.addTo(map);
 });
-  // 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
+  // Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
   d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
 
     function styleInfo(feature) {
       return {
         opacity: 1,
-        fillOpacity: 1,
+        fillOpacity: .5,
         fillColor: getColor(feature.properties.mag),
         color: "#000000",
         radius: getRadius(feature.properties.mag),
@@ -125,7 +164,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       };
     }
 
-      // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+      // Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
       function getColor(magnitude) {
         if (magnitude > 5) {
           return "#ea2c2c";
@@ -136,7 +175,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return "#98ee00";
       }
 
-      // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+      // Use the function that determines the radius of the earthquake marker based on its magnitude.
       function getRadius(magnitude) {
         if (magnitude === 0) {
           return 1;
@@ -155,14 +194,29 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         style: styleInfo,
         // We create a popup for each circleMarker to display the magnitude and location of the earthquake
         onEachFeature: function(feature, layer) {
-          layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+          //add timestamp
+          var unix_timestamp = feature.properties.time;
+          var date = new Date(unix_timestamp).toLocaleDateString("en-US");
+          var time = new Date(unix_timestamp).toLocaleTimeString("en-US");
+        layer.bindPopup("Date: " + date + "<br>Time: " + time + "<br>Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+          // event listeners
+        layer.on({
+          mouseover: function(event) {
+              let layer_target = event.target;
+              layer_target.setStyle({ fillOpacity: 1 });
+          },
+          mouseout: function(event) {
+              let layer_target = event.target;
+              layer_target.setStyle({ fillOpacity: 0.5 });
+          },
+          click: function(event) {
+              myMap.fitBounds(event.target.getBounds());
+          }
+        });
         }
     }).addTo(majorEarthquakes);
-
-    // Then we add the earthquake layer to our map.
-    majorEarthquakes.addTo(map);
   });
-  
+
     // Here we create a legend control object.
   let legend = L.control({
     position: "bottomright"
@@ -195,11 +249,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       // Finally, we our legend to the map.
       legend.addTo(map);
 
-
-      // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
+      //Use d3.json to make a call to get our Tectonic Plate geoJSON data.
       d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
         L.geoJson(data, {
-        style: {color: "#703606", weight: 3},
+        style: {color: "#aacc00", weight: 3},
         }).addTo(tectonic); 
         
       });
